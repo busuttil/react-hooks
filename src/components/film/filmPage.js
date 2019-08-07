2cb93388742fd484fe5fe0b0603b3d78d728077e
Nicolas BUSUTTIL <nbusuttil@vente-privee.com>
2019-08-07 17:33:41 +0200

    feat(test): implem test

--- src/components/film/filmPage.js
+++ src/components/film/filmPage.js
@@ -5,25 +5,28 @@ import { filmFetchReducer, initialState } from './store/film.reducer';
 import { getFilmById } from './store/film.actions';
 import { POSTER_URL } from '../../utils';
 
-function FilmPage({ filmId }) {
+export const FilmPage = ({ filmId }) => {
   const [state, dispatch] = useReducer(filmFetchReducer, initialState);
   useEffect(() => {
     let didCancel = false;
-    if(!state.filmPage.title) {
+    if (!state.filmPage.title) {
       getFilmById(dispatch, didCancel, filmId);
     }
     return () => (didCancel = true);
   });
 
-
   return (
     <div className="page page--single">
       <A href="/">Back to home</A>
       <h1>Title: {state.filmPage.title}</h1>
-      <img src={`${POSTER_URL}${state.filmPage.poster_path}`} alt={state.filmPage.poster_path} className="page__poster" />
+      <img
+        src={`${POSTER_URL}${state.filmPage.poster_path}`}
+        alt={state.filmPage.poster_path}
+        className="page__poster"
+      />
       <p>Synopsis: {state.filmPage.overview}</p>
     </div>
   );
-}
+};
 
 export default FilmPage;
